0e2654ce92e0c6ac9727198a5dee0d1a7642d9f7
mode80 <mode80@users.noreply.github.com>
2024-08-07 06:51:49 -0600

    .

--- scripts/launch_debug.sh
+++ scripts/launch_debug.sh
@@ -1,27 +1,11 @@
 #!/bin/zsh
 
-# TMP_SCRIPT="/Users/user/sand/mame0268_/launch_mame.sh"
+MAME_SCRIPT="${PWD}/scripts/launch_mame.sh"
 
-# UGLY BUT WORKS
-# Create a temporary script file
-TMP_SCRIPT=$(mktemp)
-
-# Write the MAME launch command to the temporary script
-cat << 'EOF' > $TMP_SCRIPT
-#!/bin/zsh
-cd /Users/user/sand/mame0268_ && \
-./mamed -window -skip_gameinfo -sound none -autoboot_script /Users/user/Dropbox/code/mamegym/console_wrapper.lua joust
-# echo "MAME process has exited. Press any key to close this window."
-# read -k1 -s
-EOF
-
-# Make the temporary script executable
-chmod +x $TMP_SCRIPT
-
-# Open a new Terminal window and run the temporary script
+# Open a new Terminal window and run the MAME script
 osascript <<EOF
 tell application "Terminal"
-    do script "zsh $TMP_SCRIPT"
+    do script "zsh $MAME_SCRIPT"
     activate
 end tell
 EOF
@@ -30,6 +14,3 @@ EOF
 sleep 5
 
 echo "MAME should now be running. Starting debug session..."
-
-# Clean up the temporary script
-rm $TMP_SCRIPT
