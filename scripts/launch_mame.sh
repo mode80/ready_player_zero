53dc668d88325506a7e0e9a6222aae2dd2d67745
mode80 <mode80@users.noreply.github.com>
2024-08-07 07:40:03 -0600

    .

--- scripts/launch_mame.sh
+++ scripts/launch_mame.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 cd /Users/user/sand/mame0268_ && \
 ./mamed -window -skip_gameinfo -sound none -autoboot_script /Users/user/Dropbox/code/mamegym/console_wrapper.lua joust
-echo "MAME process has exited. Press any key to close this window."
-read -k1 -s
+# echo "MAME process has exited. Press any key to close this window."
+# read -k1 -s
