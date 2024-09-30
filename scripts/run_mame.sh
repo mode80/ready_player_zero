07a38c9996225b28ea8ce3a8038361bf669c1755
mode80 <mode80@users.noreply.github.com>
2024-09-30 16:26:21 -0600

    remove scripts only relevant to other branch

--- scripts/run_mame.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-cd ~/mame && \
-./mame -window -skip_gameinfo -console -pause_brightness 1.0 -sound none -autoboot_script  /Users/user/Dropbox/code/ready_player_zero/ready_player_zero/mame_server.lua joust
